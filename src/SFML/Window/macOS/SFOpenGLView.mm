368f6ff21d5602f5d2b3ff09f449fc7f83cf72c5
Maximilian Rieger <max.rieger@tum.de>
2023-11-15 16:29:09 +0100

    set high-dpi to true on default

--- src/SFML/Window/macOS/SFOpenGLView.mm
+++ src/SFML/Window/macOS/SFOpenGLView.mm
@@ -88,13 +88,13 @@
 ////////////////////////////////////////////////////////
 - (id)initWithFrame:(NSRect)frameRect
 {
-    return [self initWithFrame:frameRect fullscreen:NO highDpi:NO];
+    return [self initWithFrame:frameRect fullscreen:NO highDpi:YES];
 }
 
 ////////////////////////////////////////////////////////
 - (id)initWithFrame:(NSRect)frameRect fullscreen:(BOOL)isFullscreen
 {
-    return [self initWithFrame:frameRect fullscreen:isFullscreen highDpi:NO];
+    return [self initWithFrame:frameRect fullscreen:isFullscreen highDpi:YES];
 }
 
 ////////////////////////////////////////////////////////
